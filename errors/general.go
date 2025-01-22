fa5b500efa872a11aeb169fa0d5f61d7eb42637e
Mohsen <mhthrh@gmail.com>
2025-01-22 15:08:28 -0800

    add an error for test

--- errors/general.go
+++ errors/general.go
@@ -1,1 +1,13 @@
 package errors
+
+import "time"
+
+func StreetEmpty() *XError {
+	return &XError{
+		Code:    "ADR100100",
+		Type:    "Address Error",
+		Message: "street name cannot be empty",
+		Details: "street name cannot be empty",
+		Time:    time.Now().String(),
+	}
+}
--- errors/model.go
+++ errors/model.go
@@ -1,7 +1,7 @@
 package errors
 
-type Error struct {
-	Code    int    `json:"code"`
+type XError struct {
+	Code    string `json:"code"`
 	Type    string `json:"type"`
 	Message string `json:"message"`
 	Details string `json:"detail"`
