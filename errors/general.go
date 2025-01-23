01891ce3e99b543224b3704151098457a7d849ae
Mohsen <mhthrh@gmail.com>
2025-01-22 16:24:53 -0800

    edit errors

--- errors/general.go
+++ errors/general.go
@@ -1,47 +1,57 @@
 package errors
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-func StreetNotFound() *XError {
+func StreetNotFound(e error) *XError {
 	return &XError{
-		Code:    "ADR100100",
-		Type:    "Address Error",
-		Message: "street name cannot be empty",
-		Details: "street name cannot be empty",
-		Time:    time.Now().String(),
+		Code:          "ADR100100",
+		Type:          "Address Error",
+		Message:       "street name cannot be empty",
+		Details:       "street name cannot be empty",
+		InternalError: fmt.Sprintf("internal error: %v", e),
+		Time:          time.Now().String(),
 	}
 }
-func PostalCodeNotFound() *XError {
+func PostalCodeNotFound(e error) *XError {
 	return &XError{
-		Code:    "ADR100101",
-		Type:    "Address Error",
-		Message: "postal code cannot be empty",
-		Details: "postal code cannot be empty",
-		Time:    time.Now().String(),
+		Code:          "ADR100101",
+		Type:          "Address Error",
+		Message:       "postal code cannot be empty",
+		Details:       "postal code cannot be empty",
+		InternalError: fmt.Sprintf("internal error: %v", e),
+		Time:          time.Now().String(),
 	}
 }
-func StateNotFound() *XError {
+func StateNotFound(e error) *XError {
 	return &XError{
-		Code:    "ADR100102",
-		Type:    "Address Error",
-		Message: "state name cannot be empty",
-		Details: "state name cannot be empty",
+		Code:          "ADR100102",
+		Type:          "Address Error",
+		Message:       "state name cannot be empty",
+		Details:       "state name cannot be empty",
+		InternalError: fmt.Sprintf("internal error: %v", e),
+		Time:          time.Now().String(),
 	}
 }
-func CityNotFound() *XError {
+func CityNotFound(e error) *XError {
 	return &XError{
-		Code:    "ADR100103",
-		Type:    "Address Error",
-		Message: "city name cannot be empty",
-		Details: "",
-		Time:    time.Now().String(),
+		Code:          "ADR100103",
+		Type:          "Address Error",
+		Message:       "city name cannot be empty",
+		Details:       "city name cannot be empty",
+		InternalError: fmt.Sprintf("internal error: %v", e),
+		Time:          time.Now().String(),
 	}
 }
-func CountryNotFound() *XError {
+func CountryNotFound(e error) *XError {
 	return &XError{
-		Code:    "ADR100104",
-		Type:    "Address Error",
-		Message: "country code cannot be empty",
-		Details: "country code cannot be empty",
+		Code:          "ADR100104",
+		Type:          "Address Error",
+		Message:       "country code cannot be empty",
+		Details:       "country code cannot be empty",
+		InternalError: fmt.Sprintf("internal error: %v", e),
+		Time:          time.Now().String(),
 	}
 }
--- errors/model.go
+++ errors/model.go
@@ -1,9 +1,10 @@
 package errors
 
 type XError struct {
-	Code    string `json:"code"`
-	Type    string `json:"type"`
-	Message string `json:"message"`
-	Details string `json:"detail"`
-	Time    string `json:"time"`
+	Code          string `json:"code"`
+	Type          string `json:"type"`
+	Message       string `json:"message"`
+	Details       string `json:"detail"`
+	InternalError string `json:"internalError"`
+	Time          string `json:"time"`
 }
