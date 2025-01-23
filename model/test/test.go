be0f8f728c7acf4bed7778cd3fddf51a7df466b3
Mohsen <mhthrh@gmail.com>
2025-01-23 12:51:15 -0800

    edit test

--- model/test/test.go
+++ model/test/test.go
@@ -1,9 +1,13 @@
 package test
 
+import (
+	"github.com/mhthrh/common-lib/errors"
+)
+
 type Test struct {
 	Name     string
 	Input    any
 	OutPut   any
 	HasError bool
-	Err      error
+	Err      *errors.XError
 }
