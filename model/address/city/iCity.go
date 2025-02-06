69f4e4c7ee294e1cd0947d82d85e52c54dc8612a
Mohsen <mhthrh@gmail.com>
2025-02-05 16:49:05 -0800

    edit for test

--- model/address/city/iCity.go
+++ model/address/city/iCity.go
@@ -10,7 +10,7 @@ type ICity interface {
 	Cities() ([]City, *cErrModel.XError)
 	GetByCountry(country string) ([]City, *cErrModel.XError)
 	GetByCity(city string) ([]City, *cErrModel.XError)
-	GetByCityAndCountry(city string, country string) (City, *cErrModel.XError)
+	GetByCityAndCountry(city string, country string) (*City, *cErrModel.XError)
 }
 type City struct {
 	ID          uuid.UUID `json:"id"`
